15529e07719c9530fe65d99127ad25c39b9466c0
Christopher Savino <christophersavino@Christophers-MacBook-Air.local>
2026-06-09 12:17:40 -0500

    Fix Application GlobalSuppressions

--- Application/GlobalSuppressions.cs
+++ Application/GlobalSuppressions.cs
@@ -1,8 +1,10 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
+// This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Design", "CA1040:Avoid empty interfaces", Justification = "<Pending>", Scope = "type", Target = "~T:Application.Abstractions.Messaging.ICommand")]
+[assembly: SuppressMessage("Design", "CA1040:Avoid empty interfaces", Justification = "Marker interfaces by design")]
+[assembly: SuppressMessage("Design", "CA1812:Avoid uninstantiated internal classes", Justification = "Instantiated via dependency injection")]
+[assembly: SuppressMessage("Performance", "CA1848:Use LoggerMessage delegates", Justification = "Pending optimization")]
+[assembly: SuppressMessage("Reliability", "CA2007:Consider calling ConfigureAwait", Justification = "ASP.NET Core does not require ConfigureAwait")]
+[assembly: SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "DTO requires settable properties for deserialization")]
